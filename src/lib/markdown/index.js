4c8cefd6602d9bb79d537d32910c6ca091439fff
Himanshu Kumar <himanshukumar077@gmail.com>
2026-02-19 12:33:41 +0530

    added two more projects

--- src/lib/markdown/index.js
+++ src/lib/markdown/index.js
@@ -2,12 +2,18 @@ import ExpenseCalculator from "./expense-calculator.md";
 import Bidmii from "./bidmii.md";
 import About from "./about.md";
 import ChartLock from "./chartlock.md";
+import Braverhood from "./braverhood.md";
+import SmartContactAI from "./smartcontact-ai.md";
+import SnapbackReturns from "./snapback-returns.md";
 
 export const MarkdownFilesMap = {
   1: Bidmii,
   2: ExpenseCalculator,
   3: About,
   chartlock: ChartLock,
+  braverhood: Braverhood,
+  "smartcontact-ai": SmartContactAI,
+  "snapback-returns": SnapbackReturns,
 };
 
 export const fetchMarkDownFile = (id) => {
